b333d5e7e40c15881ece8e602f5e42e0fae2d8f1
Sudikulal <sudikulal325@gmail.com>
2024-01-13 11:54:24 +0530

    Update:added express static page

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const path  = require("path")
 
 require("dotenv").config();
 
@@ -13,6 +14,8 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(express.static(path.join(__dirname, "./public")));
+
 require("./src/db/mongoose");
 const userRoute = require("./src/routes/user.route");
 const toolRoute = require("./src/routes/tool.route");
@@ -23,17 +26,17 @@ app.use(toolRoute);
 app.get("/", (req, res) => res.send("hello"));
 
 // Create Telegraf bot
-const bot = require("./src/util/telegraf.util");
+// const bot = require("./src/util/telegraf.util");
 
-// require("./src/util/bot.util")
+require("./src/util/bot.util")
 
-// Set up a webhook endpoint for receiving updates
-app.post(`/bot${process.env.BOT_TOKEN}`, (req, res) => {
-  bot.handleUpdate(req.body, res);
-});
+// // Set up a webhook endpoint for receiving updates
+// app.post(`/bot${process.env.BOT_TOKEN}`, (req, res) => {
+//   bot.handleUpdate(req.body, res);
+// });
 
 // Start the Express server
 app.listen(port, () => {
   console.log(`Express server is listening on port ${port}`);
-  // bot.launch().then(() => console.log('Bot is running...'));
+  //  bot.launch().then(() => console.log('Bot is running...'));
 });
